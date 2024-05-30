d9289f66112c80916ab479849bf1008505c60c9d
Christan <rajakoonechristan@gmail.com>
2024-05-31 02:54:18 +0530

    Responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,22 +10,24 @@ export default function NavBar() {
     
     return (
       
-      <>
       
-      <img className='logo' src={logo}/>
-      
-        <div className='navbar'>
-            
-            <Stack spacing={2} direction="row">
-            <BasicButtons hovercolor = "#858383" bgcolor = '#FFFFFF' name = "Home" fontcolor="black"/>
-            <BasicButtons hovercolor = "#858383" bgcolor = '#FFFFFF' name = "About" fontcolor= "black"/>
-            <BasicButtons hovercolor = "#858383" bgcolor = '#FFFFFF' name = "Contact" fontcolor = "black"/>
-            <BasicButtons hovercolor = "#858383" bgcolor = '#6A41AE' name = "Sign In"/>
-            </Stack>
-            <hr className='hr1'/>
-        </div>
-
-       
-      </>
+      <div>
+        
+          <img className='logo' src={logo}/>
+          
+            <div className='navbar'>
+          
+                <Stack direction={{ xs: 'column', sm: 'row' }}
+                        spacing={{ xs: 1, sm: 2, md: 4 }}>
+                <BasicButtons hovercolor = "#858383" bgcolor = '#FFFFFF' name = "Home" fontcolor="black"/>
+                <BasicButtons hovercolor = "#858383" bgcolor = '#FFFFFF' name = "About" fontcolor= "black"/>
+                <BasicButtons hovercolor = "#858383" bgcolor = '#FFFFFF' name = "Contact" fontcolor = "black"/>
+                <BasicButtons hovercolor = "#858383" bgcolor = '#6A41AE' name = "Sign In"/>
+                </Stack>
+                <hr className='hr1'/>
+            </div>
+          
+      </div>
+     
     );
   }
